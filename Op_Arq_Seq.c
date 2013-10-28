08ccb28e37dbea979e5d2ebbb67f5a5b11c5498c
Raphael Barreto <rfsbarreto@gmail.com>
2013-10-28 16:46:57 -0300

    Add Leitura de campos e escritano arquivo

--- Op_Arq_Seq.c
+++ Op_Arq_Seq.c
@@ -2,11 +2,21 @@
 
 int main(){
 	FILE* fout;
-	fout=fopen("saida.txt","r");
+	fout=fopen("saida.txt","w");
 	
 	if (!fout){
 		printf("Erro ao abrir arquivo");
 		return -1;
 	}
-	
+	int numRegistros,numCampos,j,i;
+	scanf("Informe o numero de registros: %d \n",&numResgistros);
+	scanf("Informe o numero de campos: %d \n",&numCampos);
+	char* campos = malloc(15*sizeof(char));
+	for (j=0;j<numRegistros;j++){
+		for (i=0;i<numCampos;i++){
+			scanf("Digite Campo %d: %15s\n",&campos);
+			fprintf(fout,"%15s|");
+		}
+		fprintf(fout,"\n");
+	}
 }
