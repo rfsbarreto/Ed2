e5f3c53ea52c92d40d408d76661fc36ca98e4a32
Raphael Barreto <rfsbarreto@gmail.com>
2013-10-29 02:28:09 -0300

    add funcionalidade de consulta por matricula

--- Op_Arq_Seq.c
+++ Op_Arq_Seq.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-//questao 1
+typedef struct func{
+	char num_mat[5];
+	int id;
+	char nome[15];
+	char cargo[15];
+	int escolaridade;
+	char sexo;
+	char local[15];
+	char est_civil;
+	int salario;
+} func;
+	
+//questao 1 escrita de arquivo generica
 int escrita_arq_sequencial(char* nome_arquivo){
 	FILE* fout;
 	fout=fopen(nome_arquivo,"w");
@@ -27,7 +39,7 @@ int escrita_arq_sequencial(char* nome_arquivo){
 	return 0;
 }
 
-//questao 2
+//questao 2 leitura arquivo com 2 campos
 int leitura_arq_sequencial(char* nome_arquivo){
 	FILE* fin = fopen(nome_arquivo,"r");
 	if (!fin){
@@ -54,8 +66,42 @@ int get_qtd_campos(char* nome_arquivo){
 	return cont;
 }
 
-int main(){
-	escrita_arq_sequencial("saida.txt");
-	leitura_arq_sequencial("saida.txt");
+func* leitura_funcionario(FILE* file){
+	func* f;
+	f= malloc(sizeof(func));
+	if (fscanf(file,"%d%*c%5[^|]|%15[^|]|%15[^|]|%d|%c|%15[^|]|%c|%d",&(*f).id,(*f).num_mat,(*f).nome,(*f).cargo,&(*f).escolaridade,&(*f).sexo,(*f).local,&(*f).est_civil,&(*f).salario)==9){
+		imprime_func(*f);
+		return f;
+	}else
+		return NULL;
+}
 
+void imprime_func(func f){
+	printf("Funcionario: %d %s %s %s %d %c %s %c %d\n",f.id,f.num_mat,f.nome,f.cargo,f.escolaridade,f.sexo,f.local,f.est_civil,f.salario);
+}
+
+//questao 3
+int consulta_arq_sequencial(char* nome_arquivo,char* matricula,char sexo, int sal){
+	func* temp;
+	FILE* f = fopen("questao3.txt","r");
+	
+	if (matricula!= NULL){
+		temp= leitura_funcionario(f);
+		if ((*temp).num_mat==matricula)
+			return 1;
+		else
+			return 0;
+	}else {
+		//if (
+	}	 
+	return 1;
+}
+
+
+
+int main(){
+//	escrita_arq_sequencial("saida.txt");
+	leitura_arq_sequencial("questao3.txt");
+	FILE* f = fopen("questao3.txt","r");
+	leitura_funcionario(f);
 }
